33a2e7ba69d7ad8409e5d36593fa61e23bf493b0
Ana Maria <anamarych@gmail.com>
2022-10-05 23:24:09 +0200

    [Broker] Needed files for the broker to execute

---
+++ Python/siotcom/src/broker/mongo.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+
+# 2022 SYST, Universidad Politecnica de Madrid
+# SIoTCom
+# @File : mongo.py
+# @Author : a.cvillasenor@alumnos.upm.es
+# This takes data from source and connects to MongoDB
+
+# Local imports
+
+# External imports
+import pymongo
+import json
+from pymongo.errors import ConnectionFailure
+
+# Global Config
+MONGO_URI = "mongodb://ip:port/" # mongodb://ip:port because we are connecting remotely
+MONGO_DB = "siotcom"
+MONGO_COLLECTION = "sensor"
+
+class Mongo():
+    def __init__(self):
+        self.client: pymongo.MongoClient = None
+        self.database: pymongo.database.Database = None
+        self.collection: pymongo.collection.Collection = None
+        
+    def connect(self):
+        self.client = pymongo.MongoClient(MONGO_URI)
+        self.database = self.client[MONGO_DB]
+        self.collection = self.database[MONGO_COLLECTION]
+        print("Connecting to MongoDB")
+
+    def on_Connect(self):
+        if self.client:
+            try:
+                # The ismaster command does not require auth
+                self.client.admin.command("hello")
+                print("Connected to MongoDB")
+                return True
+            except ConnectionFailure as cf:
+                print("Connection Refused")
+                print(cf)
+                return False
+        else:
+            return False
+
+    def disconnect(self):
+        if self.client:
+            self.client.close()
+            self.client = None
+        print("Disconnected from MongoDB")
+    
+    def save(self, msg):
+        if self.on_Connect():
+            try:
+                result = self.collection.insert_one(json.loads(msg))
+                print("Saved document with id ", result.inserted_id)
+            except Exception as ex:
+                print(ex)
+        else:
+            print("Could not store data in MongoDB")
---
+++ Python/siotcom/src/broker/mqtt.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+
+# 2022 SYST, Universidad Politecnica de Madrid
+# SIoTCom
+# @File : mqtt.py
+# @Author : a.cvillasenor@alumnos.upm.es
+# Configuration of MQTT behaviour
+
+# Local imports
+from azureBase import AzureBase
+from mongo import Mongo
+
+# External imports
+import paho.mqtt.client as mqtt
+
+# Global Configuration
+MQTT_HOST = "localhost" # este concentrador funciona como broker
+MQTT_KEEPALIVE = 60
+MQTT_PORT = 1883 # puerto default
+
+class MQTT():
+    def __init__(self, client_id, mongo: Mongo = None, az: AzureBase = None):
+        self.mongo: Mongo = mongo
+        self.az: AzureBase = az
+        self.mqtt_client = mqtt.Client(client_id) #create client
+        self.mqtt_client.on_connect = self.on_connect #on_connect callback
+        self.mqtt_client.on_message = self.on_message #on_message callback
+        self.mqtt_client.on_publish = self.on_publish #on_publish callback
+        self.mqtt_client.on_disconnect = self.on_disconnect #on_disconnect callback
+
+    def on_connect(self, client:mqtt.Client, userdata, flags, rc):
+        if rc == 0:
+            print("Connected to Mosquitto")
+        else:
+            print("Failed to connect to Mosqutito")
+    
+    def on_disconnect(self, client:mqtt.Client, userdata, rc=0):
+        print("Disconnected from Mosquitto")
+        
+    def on_message(self, client: mqtt.Client, userdata, msg):
+        if self.mongo is not None:
+            self.mongo.save(msg.payload)
+        else:
+            self.az.send_data(msg.payload)            
+    
+    def on_publish(self, client:mqtt.Client, data, mid):
+        print("Published", mid)
+
+    def publish(self, topic, data):
+        self.mqtt_client.publish(topic, data)
+    
+    def subscribe(self, topic, qos):
+        self.mqtt_client.subscribe(topic, qos)
+        print("Suscribed to ", topic)
+    
+    def run(self):
+        self.mqtt_client.connect(MQTT_HOST, MQTT_PORT, MQTT_KEEPALIVE)
+        self.mqtt_client.loop_start()
+
+    def stop(self):
+        self.mqtt_client.loop_stop()
+        self.mqtt_client.disconnect()
